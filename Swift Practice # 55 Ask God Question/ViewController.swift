57ab34f1536474206d1e1da349241c99497d889d
DogpaChen <dogpa0130@gmail.com>
2021-08-08 14:02:57 +0800

    Initial Commit

---
+++ Swift Practice # 55 Ask God Question/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 55 Ask God Question
+//
+//  Created by CHEN PEIHAO on 2021/8/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
